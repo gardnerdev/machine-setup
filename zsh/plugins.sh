a784acf1e0f09a138492192f44c2f63b416733d6
Rafal Chojnacki <rafalchojnacki0502@gmail.com>
2022-07-02 10:57:23 +0000

    zsh plugins

--- zsh/plugins.sh
+++ zsh/plugins.sh
@@ -1,9 +1,17 @@
+ZSH_CUSTOM=~/.oh-my-zsh/custom
+AUTO_SUG_DIR=~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+SYNTAX_DIR=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+SPACESHIP_DIR="$ZSH_CUSTOM/themes/spaceship-prompt"
+FZF_DIR="~/.fzf"
+VIRTUAL_DIR="$ZSH_CUSTOM/plugins/autoswitch_virtualenv"
+ZGEN_DIR="${HOME}/.zgen"
+
 echo "Installing plugins"
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-ln -sf "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-git clone "https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv.git" "$ZSH_CUSTOM/plugins/autoswitch_virtualenv"
-git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+git -C $AUTO_SUG_DIR https://github.com/zsh-users/zsh-autosuggestions pull || git clone $AUTO_SUG_DIR  && \
+git -C $SYNTAX_DIR https://github.com/zsh-users/zsh-syntax-highlighting.git  pull || git clone $SYNTAX_DIR && \
+git -C $SPACESHIP_DIR https://github.com/spaceship-prompt/spaceship-prompt.git pull || git clone  $SPACESHIP_DIR --depth=1 && \
+ln -sf "${SPACESHIP_DIR}/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme" && \
+git -C $FZF_DIR --depth 1 https://github.com/junegunn/fzf.git pull || git clone $FZF_DIR && \
+${FZF_DIR}/install && \
+git -C $VIRTUAL_DIR https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv.git pull || git clone $VIRTUAL_DIR && \
+git -C &ZGEN_DIR https://github.com/tarjoilija/zgen.git pull || git clone $ZGEN_DIR

481e397197222d57ebc16c0d54ec31445e76d0dc
Rafal Chojnacki <rafalchojnacki0502@gmail.com>
2022-07-02 15:53:43 +0000

    poetry added after zsh

--- poetry/setup.sh
+++ poetry/setup.sh
@@ -1,7 +1,1 @@
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
-# Zsh
-poetry completions zsh > ~/.zfunc/_poetry
-
-# Oh-My-Zsh
-mkdir $ZSH_CUSTOM/plugins/poetry
-poetry completions zsh > $ZSH_CUSTOM/plugins/poetry/_poetry

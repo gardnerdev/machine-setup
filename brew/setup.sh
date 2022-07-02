75516d65b4a9e2bb92e991ffb7b310bb44d46958
Rafal Chojnacki <rafalchojnacki0502@gmail.com>
2022-07-02 15:04:36 +0000

    brew modifications

--- brew/setup.sh
+++ brew/setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/users/chojnar1/.profile
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && \
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/users/chojnar1/.zprofile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew)"
+export BREW_HOME="/home/linuxbrew/.linuxbrew/bin"
+export PATH="$PATH:$BREW_HOME"

ccb0630fc5544812f112fedfa6c456ebcf217172
Rafal Chojnacki <rafalchojnacki0502@gmail.com>
2022-07-02 11:21:08 +0000

    new plugins setup

--- zsh/plugins.sh
+++ zsh/plugins.sh
@@ -11,12 +11,12 @@ ZGEN_DIR="${HOME}/.zgen"
 echo "Installing plugins"
 git -C $AUTO_SUG_DIR pull || git clone https://github.com/zsh-users/zsh-autosuggestions $AUTO_SUG_DIR  && \
 git -C $SYNTAX_DIR pull || git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $SYNTAX_DIR && \
-git -C $SPACESHIP_DIR pull || git clone --depth=1 https://github.com/spaceship-prompt/spaceship-prompt.git $SPACESHIP_DIR && \
+git -C $SPACESHIP_DIR pull || git clone https://github.com/spaceship-prompt/spaceship-prompt.git $SPACESHIP_DIR --depth=1 && \
 ln -sf "${SPACESHIP_DIR}/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme" && \
-git -C $FZF_DIR --depth 1 pull || git clone https://github.com/junegunn/fzf.git $FZF_DIR && \
+git -C $FZF_DIR pull || git clone https://github.com/junegunn/fzf.git $FZF_DIR --depth=1 && \
 ${FZF_DIR}/install && \
 git -C $VIRTUAL_DIR pull || git clone https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv.git $VIRTUAL_DIR && \
-git -C $ZGEN_DIR pull || git clone https://github.com/tarjoilija/zgen.git pull $ZGEN_DIR && \
+git -C $ZGEN_DIR pull || git clone https://github.com/tarjoilija/zgen.git $ZGEN_DIR && \
 
 echo "Copying zsh setup"
 cp .zshrc ~/
